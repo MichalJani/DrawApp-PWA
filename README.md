5daff8b26411d325f61ce1362b277b9694190eb0
Michal <michal.janicki1@gmail.com>
2020-06-06 20:34:06 +0200

    Remove unnecessary files
    Signed-off-by: Michal <michal.janicki1@gmail.com>

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 This app provides a robust yet simple solution supporting decision making process.
 
-It is a Progressive Web App, which means that it can be downloaded to you mobile or desktop and work similar to native apps.
-
-It was inspired by me and a friend of mine as we struggled to decide whether we should get absolutely hammered that night.
-It also guided our subsequent choices.
-
-You can dwonload the app from here[https://drawappmichal.firebaseapp.com/].
-
-To download it on Android go to settings and add to home screen.
-To download it on iPhone go to sharing options and choose to add it to your device.
-
+It is a Progressive Web App, it can be downloaded to you mobile or desktop and work similar to native apps.
 
 `npm start` - runs the project on the localhost:3000
 `npm run build` - builde the project into the build directory
--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Button } from 'reactstrap';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -14,23 +13,23 @@ class App extends Component {
   calculateDraw = () => {
     const randomNumber = Math.random();
     if (randomNumber < 0.5) {
-      return 'no'
+      return 'no';
     } else if (randomNumber < 1) {
-      return 'yes'
+      return 'yes';
     }
-  }
+  };
 
   handleDrawClick = () => {
     this.setState({
       draw: true,
-    })
+    });
+  };
 
-  }
   handleDrawDrawAgainClick = () => {
     this.setState({
       draw: false,
-    })
-  }
+    });
+  };
 
   renderDrawOutcome = () => {
     let drawOutcome = 'I dont know';
@@ -38,59 +37,61 @@ class App extends Component {
     const draw = this.calculateDraw();
     switch (draw) {
       case 'yes':
-        drawOutcome = 'Yes, do it!'
+        drawOutcome = 'Yes, do it!';
         break;
       case 'no':
-        drawOutcome = 'No, don\'t do it'
-        backgroundColor = 'red'
+        drawOutcome = "No, don't do it";
+        backgroundColor = 'red';
         break;
       default:
-        console.log('dont know');
+        return;
     }
+
     return (
-      <div className="backgroundColor" style={{
-        background: backgroundColor
-      }}>
-        <div className="button_container">
-          <div className="outcome_text">
+      <div
+        className='backgroundColor'
+        style={{
+          background: backgroundColor,
+        }}
+      >
+        <div className='button_container'>
+          <div className='outcome_text'>
             <span>{drawOutcome}</span>
           </div>
           <Button
-            type="button"
+            type='button'
             className='drawAgain_button'
-            color="secondary"
+            color='secondary'
             onClick={() => this.handleDrawDrawAgainClick()}
           >
             Draw again
-        </Button>
-        </div >
+          </Button>
+        </div>
       </div>
-    )
-  }
+    );
+  };
 
   renderButton = () => {
     return (
-      <div className="button_container">
-        <div className="draw_text">
-          <span>
-            Yes or No?
-          </span>
+      <div className='button_container'>
+        <div className='draw_text'>
+          <span>Yes or No?</span>
         </div>
         <Button
-          type="button"
-          className="draw_button"
-          color="success"
+          type='button'
+          className='draw_button'
+          color='success'
           onClick={() => this.handleDrawClick()}
         >
           Draw
         </Button>
       </div>
-    )
-  }
+    );
+  };
 
   render() {
     return (
-      <div className="app_container">
+      <div className='app_container'>
         {this.state.draw ? this.renderDrawOutcome() : this.renderButton()}
       </div>
     );
